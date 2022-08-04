3b6d1b37389d6028201562a3983c4b1a3aa9a94e
aartaka <aartaka@protonmail.com>
2022-08-04 12:06:53 +0400

    auto-rules(apply-auto-rules): Abstract the rule application.
    And use it in renderer/gtk.lisp.

--- source/renderer/gtk.lisp
+++ source/renderer/gtk.lisp
@@ -1005,22 +1005,7 @@ See `finalize-buffer'."
       (setf modifiers (funcall (modifier-translator *browser*)
                                (webkit:webkit-navigation-action-get-modifiers navigation-action))))
     (setf url (quri:uri (webkit:webkit-uri-request-uri request)))
-    (unless (bypass-auto-rules-p buffer)
-      (let* ((rules (matching-auto-rules url buffer))
-             (previous-url (previous-url buffer))
-             (previous-rules (when previous-url (matching-auto-rules previous-url buffer))))
-        (when (and rules previous-url (not previous-rules))
-          (save-last-active-modes buffer previous-url))
-        (cond
-          ((and (not rules)
-                ;; `toplevel-p'
-                (quri:uri=
-                 url (quri:uri (webkit:webkit-web-view-uri
-                                (gtk-object buffer)))))
-           (reapply-last-active-modes buffer))
-          ((and rules (not (eq rules previous-rules)))
-           (enable-matching-modes url buffer)))
-        (setf (previous-url buffer) url)))
+    (apply-auto-rules url buffer)
     (let* ((request-data
             (hooks:run-hook
              (request-resource-hook buffer)
